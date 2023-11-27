b4bbecbf88d98f92e4b7028da44facfda79c5806
Simone-Ramundi <simoneramundi@outlook.com>
2023-11-27 14:13:39 +0000

    Reformatting code using async pipe

--- src/app/weather/forecast/forecast.component.ts
+++ src/app/weather/forecast/forecast.component.ts
@@ -1,20 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ForecastService } from '../forecast.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
   styleUrls: ['./forecast.component.css']
 })
 export class ForecastComponent implements OnInit {
-
-  forecastData = [];
+  forecast$: Observable<{ dateString: string; temp: number;}[]>
 
   constructor(forecastService: ForecastService) { 
-    forecastService.getForecast()
-    .subscribe((forecastData)=>{
-      // console.log(forecastData);
-      this.forecastData = forecastData;
-    })
+    this.forecast$ = forecastService.getForecast()
   }
 
   ngOnInit() {
