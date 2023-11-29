549c8c18592594c32eb8b8e352a037291ec5266b
Simone-Ramundi <simoneramundi@outlook.com>
2023-11-29 14:10:52 +0000

    creating paginator comp and shared mod

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaginatorComponent } from './paginator/paginator.component';
+
+
+
+@NgModule({
+  declarations: [PaginatorComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
